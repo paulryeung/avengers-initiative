a4795fd69620eaa59aec64e1a9511e34ba6817ea
Paul Yeung <paul.r.yeung@gmail.com>
2022-07-04 14:34:16 -0400

    show list of avenger in index after adding

--- controllers/avengers.js
+++ controllers/avengers.js
@@ -3,16 +3,29 @@ const Avenger = require("../models/avenger");
 //main list of avengers page
 function index(req, res) {
   console.log("Made it to Index");
-  res.render("avengers/index");
+  Avenger.find({}, function (err, avengers) {
+    res.render("avengers/index", { avengers });
+  });
 }
 
 //page for new avengers
 function newAvenger(req, res) {
+  //find all the avengers and pass it
+
   res.render("avengers/new");
 }
 
 function add(req, res) {
   console.log("Here are form parameters", req.body);
+
+  let newAvenger = Avenger(req.body);
+  newAvenger.save(function (err) {
+    if (err) {
+      console.log(err);
+      console.log("Error adding avenger, redirecting now to page again");
+    }
+  });
+
   res.redirect("/avengers");
 }
 
