bbf245e755ed938d097e2d5776be376278ed6ebf
Paul Yeung <paul.r.yeung@gmail.com>
2022-07-04 20:38:03 -0400

    add missions request form page and routing

--- controllers/missions.js
+++ controllers/missions.js
@@ -2,9 +2,14 @@ const mission = require("../models/mission");
 
 function index(req, res) {
   console.log("Made it here to missions index controller");
-  res.render("missions/index.ejs");
+  res.render("missions/index");
+}
+
+function newMission(req, res) {
+  res.render("missions/new");
 }
 
 module.exports = {
   index,
+  new: newMission,
 };
--- routes/missions.js
+++ routes/missions.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 let missionsCtrl = require("../controllers/missions");
 
+//missions index
 router.get("/", missionsCtrl.index);
+
+//Form to add new Mission
+router.get("/new", missionsCtrl.new);
 
 module.exports = router;
