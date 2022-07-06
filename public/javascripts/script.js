65f37a4809ac2f753a99c125e2d7ffbff304ded3
Paul Yeung <paul.r.yeung@gmail.com>
2022-07-05 22:25:04 -0400

    improve abilities on edit page

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,16 +3,18 @@ console.log("Hello World Avengers!");
 //array of abilities is an array of strings
 let abilities = [];
 
-//button for add ability
+//button for add ability for a new page
 let addAbilityEl = document.querySelector("#add-ability");
+
+//the rest is the same
 let inputAbilityEl = document.querySelector("#input-ability-1");
 let divAbilityEl = document.querySelector(".abilities-div");
 let formAbilityEl = document.querySelector("#avengers-recruitment");
 
-//there is one ability by default
+//initialize counter for new recruits
 let abilityCounter = 2;
 
-//if button is found, setup the event listener so no errors
+//if button is found on new page, setup the event listener so no errors
 if (addAbilityEl) {
   //eventlistener for button
   addAbilityEl.addEventListener("click", function (evt) {
@@ -40,5 +42,41 @@ if (addAbilityEl) {
       divAbilityEl.appendChild(newBreakEl);
       abilityCounter++;
     }
+  });
+}
+
+//button for add ability for edit page, its logically different
+let addAbilityEditEl = document.querySelector("#add-ability-edit");
+
+//if button is found on edit page, setup the event listener so no errors
+if (addAbilityEditEl) {
+  //eventlistener for button
+  addAbilityEditEl.addEventListener("click", function (evt) {
+    console.log("Ha this button works!");
+
+    console.log("The counter on button is: ", evt.target.getAttribute("count"));
+
+    //only run stuff if 4 or less ability fields
+    // if (abilityCounter <= 4) {
+    //   //create an extra label
+    //   let newLabelEl = document.createElement("label");
+    //   newLabelEl.innerHTML = `Ability ${abilityCounter} `;
+
+    //   //create new input
+    //   let newInputEl = document.createElement("input");
+    //   newInputEl.setAttribute("id", `input-ability-${abilityCounter}`);
+    //   newInputEl.setAttribute("type", "text");
+    //   newInputEl.setAttribute("name", `ability${abilityCounter}`);
+
+    //   console.log("This is new input: ", newInputEl);
+    //   //create a line break
+    //   let newBreakEl = document.createElement("br");
+
+    //   //append child to div
+    //   divAbilityEl.appendChild(newLabelEl);
+    //   divAbilityEl.appendChild(newInputEl);
+    //   divAbilityEl.appendChild(newBreakEl);
+    //   abilityCounter++;
+    // }
   });
 }
